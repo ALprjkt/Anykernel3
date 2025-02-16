3b07e6b4301fe6231758c70d178c7c49b1e806d3
AL <168145100+k4ngcaribug@users.noreply.github.com>
2025-02-16 13:19:05 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=NekoLite Kernel by k4ngcaribug @ telegram
+kernel.string=Venom Kernel by k4ngcaribug @ telegram
 do.devicecheck=1
 do.modules=1
 do.systemless=0
